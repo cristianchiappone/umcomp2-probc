5356f21b5898cbda8d1b12cab09335fcab04ab75
Cristian Chiappone <cristianchiappone@outlook.com>
2019-05-09 16:24:01 -0300

    Renombrando funciones, mejorando login, agregando parseador de body http

--- servidor_pthread/http_parser.h
+++ servidor_pthread/http_parser.h
@@ -2,6 +2,6 @@
 #define _MY_HTTP_PARSER_H_
 //#define
 
-void parser(int sdc, char *buff);
+void parser_header(int sdc, char *buff);
 
 #endif
--- servidor_pthread/service.c
+++ servidor_pthread/service.c
@@ -17,15 +17,37 @@
 #include "http_parser.h"
 #include "pthread.h"
 
+const char *err_500 =
+    "HTTP/1.1 500 INTERNAL SERVER ERROR\nContent-type: "
+    "text/html\nContent-Length: 25\n\n500 INTERNAL SERVER ERROR";
+
 int service(int sdc) {
-    char buff[2048];
+    char buff[2048], copy[2048];
+    char *token, *string, *tofree;
     int leido;
+    memset(buff, 0, sizeof buff);
+    leido = read(sdc, buff, sizeof buff);
 
-    if ((leido = read(sdc, buff, sizeof buff)) == -1) {
-        printf("error %s\n", strerror(errno));
+    switch (leido) {
+        case 0:
+            printf("error \n");
+            close(sdc);
+            pthread_exit(NULL);
+            break;
+        case -1:
+            printf("error %s\n", strerror(errno));
+            close(sdc);
+            pthread_exit(NULL);
+            break;
+        case 2:
+            write(sdc, err_500, strlen(err_500));  // telnet vacio
+            close(sdc);
+            pthread_exit(NULL);
+            break;
+        default:
+            parser_header(sdc, buff);
+            break;
     }
-
-    parser(sdc, buff);
 
     return 0;
 }
